b992acc80547ae6898c5ab2e4732872560bf2262
Hisham Ismail <mhishami@gmail.com>
2019-08-12 04:40:48 +0800

    Done rename token test

--- test/mexctoken.js
+++ test/mexctoken.js
@@ -11,4 +11,14 @@ contract("MEXCToken", accounts => {
     assert.equal("MEXC Token", name, "Symbol should be MEXC Token");
     assert.equal("MEXC", symbol, "Symbol should be MEXC");
   });
+
+  it("should rename the token name and symbol", async () => {
+    let mexc = await MEXCToken.deployed();
+    await mexc.renameToken("FOO", "FOO Token");
+    let name = await mexc.name();
+    let symbol = await mexc.symbol();
+
+    assert.equal("FOO Token", name, "Symbol should be FOO Token");
+    assert.equal("FOO", symbol, "Symbol should be FOO");
+  });
 });
