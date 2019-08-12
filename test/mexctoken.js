204562c687fae299e9b1e249d0d895b392363615
Hisham Ismail <mhishami@gmail.com>
2019-08-12 16:48:50 +0800

    Fixed the supply test

--- test/mexctoken.js
+++ test/mexctoken.js
@@ -1,6 +1,13 @@
 const MEXCToken = artifacts.require("MEXCToken");
 
 contract("MEXCToken", accounts => {
+  let owner = accounts[0];
+  let acc1 = accounts[1];
+  let acc2 = accounts[2];
+  let acc3 = accounts[3];
+
+  const toWei = web3.utils.toWei;
+
   it("should have the right token name and symbol", async () => {
     // let name = await mexc.name();
     // let symbol = await mexc.symbol();
@@ -25,7 +32,17 @@ contract("MEXCToken", accounts => {
   it("should have a max supply of 1714285714", async () => {
     let mexc = await MEXCToken.deployed();
     let ts = await mexc.maxSupply();
+    let cs = await mexc.totalSupply();
 
-    assert.equal(1714285714, ts.toNumber(), "Max supply should be 1714285714");
+    assert.equal(
+      1714285714 * 10 ** 18,
+      toWei("1714285714", "ether"),
+      "Max supply should be 1714285714"
+    );
+    assert.equal(
+      0 * 10 ** 18,
+      toWei("0", "ether"),
+      "Max supply should be 1714285714"
+    );
   });
 });
